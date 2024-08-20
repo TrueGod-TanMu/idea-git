ea42419d2513d43b1874ce7d78a563bde3807668
TanMu <tanmu@163.com>
2024-08-20 17:26:23 +0800

    three idea git commit

--- src/main/java/git.java
+++ src/main/java/git.java
@@ -1,7 +1,8 @@
 public class git {
     public static void main(String[] args) {
-        System.out.println(324);
+        System.out.println("git 1");
         System.out.println("git 2");
         System.out.println("git 3");
+        System.out.println("git hot 4");
     }
 }
